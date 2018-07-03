3b9a7cf9aaec393dfb633e054f6c2de61621472c
ole <3097359+olexandrpi@users.noreply.github.com>
2018-07-03 23:26:52 +0200

    Improved some verification functionality.

--- cert_check/cert_check.py
+++ cert_check/cert_check.py
@@ -12,7 +12,7 @@ from urllib.parse import urlparse as urlparser
 class CertChecker:
     '''Verifies the TLS/SSL certificate of a website.'''
 
-    logger = logging.getLogger('cert-check')
+    logger = logging.getLogger('cbb.cert-check')
 
     def __init__(self, url):
         # see also https://stackoverflow.com/a/7691293
@@ -20,7 +20,8 @@ class CertChecker:
         self.content = self._get_content()
 
     def check(self):
-        print('Initialising TLS/SSL certificate check ...')
+        '''Main publick check function.'''
+        print('\n\n Initialising TLS/SSL certificate check ...')
         if not self.domain:
             return
 
@@ -28,15 +29,18 @@ class CertChecker:
             self._verify_version(),
             self._verify_date(),
             self._verify_subject(),
-            self._verify_issuer(),
+#            self._verify_issuer(),
             self._get_extensions(),
         ))
 
     def _get_domain(self, url):
-        print('Getting domain ...')
+        '''Extracts domain name from the URL.'''
+        print('\n Getting domain ...')
         if url.startswith('http'):
             parsed = urlparser(url)
-            return parsed.netloc
+            domain = parsed.netloc
+            print('Domain: {0}'.format(domain))
+            return domain
         else:
             self.logger.warning('URL must start with scheme (http / https)')
             return
@@ -46,8 +50,7 @@ class CertChecker:
         See https://cryptography.io/en/latest/x509/reference/#x-509-certificate-object
         for more details.
         '''  # noqa: E501
-
-        print('Getting certificate ...')
+        print('\n Getting certificate ...')
         # https://stackoverflow.com/a/16899645
         cert = ssl.get_server_certificate((self.domain, 443))
         bycert = cert.encode('utf-8')
@@ -56,18 +59,20 @@ class CertChecker:
         return content
 
     def _verify_version(self):
-        print('Verifying certificate version ...')
+        '''Verifies certificate version.'''
+        print('\n Verifying certificate version ...')
         try:
             version = self.content.version
             print('Version: {0}'.format(version))
-            return version
+            return True
         except x509.InvalidVersion as ex:
             print('Invalid certificate version: {0}'.format(ex))
             self.logger.warning('Invalid certificate version: {0}'.format(ex))
             return
 
     def _verify_date(self):
-        print('Verifying certificate dates ...')
+        '''Verifies certificate date.'''
+        print('\n Verifying certificate dates ...')
         # get notBefore
         not_before = self.content.not_valid_before
         # get notAfter
@@ -84,30 +89,37 @@ class CertChecker:
 
     # TODO
     def _verify_subject(self):
-        print('Verifying certificate subject ...')
+        print('\n Verifying certificate subject ...')
         for attribute in self.content.subject:
             key = attribute.oid._name
             val = attribute.value
             print('{0}: {1}'.format(key, val))
+            # check if domain name coinsides with the Common Name
+            if key == 'commonName' and self.domain.lower() in val.lower():
+                print(self.domain, val)
+                return True
+
+        return
+
+    # TODO
+    def _get_extensions(self):
+        '''Gets data from extensions.'''
+        print('\n Verifying certificate extensions ...')
+        san = self.content.extensions.get_extension_for_class(
+            x509.SubjectAlternativeName)
+        print('SAN: {0}'.format(san.value.get_values_for_type(x509.DNSName)))
 
     # TODO
     def _verify_issuer(self):
-        print('Verifying certificate issuer ...')
+        # http://www.sos.ca.gov/administration/regulations/current-regulations/technology/digital-signatures/approved-certification-authorities/
+        print('\n Verifying certificate issuer ...')
         for attribute in self.content.issuer:
             key = attribute.oid._name
             val = attribute.value
             print('{0}: {1}'.format(key, val))
 
-    # TODO
-    def _get_extensions(self):
-        '''Gets data from extensions.'''
-
-        print('Verifying certificate extensions ...')
-        san = self.content.extensions.get_extension_for_class(
-            x509.SubjectAlternativeName)
-        print('SAN: {0}'.format(san.value.get_values_for_type(x509.DNSName)))
-
 
 if __name__ == '__main__':
     CertChecker('https://google.com').check()
+    CertChecker('https://yahoo.com').check()
 #    CertChecker('http://admkant.nl/').check()
