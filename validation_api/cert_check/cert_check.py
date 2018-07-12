881768228e505edd9927fa2ba0579bd180027db1
ole <3097359+olexandrpi@users.noreply.github.com>
2018-07-12 23:30:45 +0200

    Added connection test, improved check if the cert does not have CN.

--- validation_api/cert_check/cert_check.py
+++ validation_api/cert_check/cert_check.py
@@ -2,8 +2,10 @@
 
 import datetime
 import logging
+import nmap
 import pytz
 import requests
+import socket
 import ssl
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
@@ -14,28 +16,38 @@ class CertChecker:
     '''Verifies the TLS/SSL certificate of a website.'''
 
     logger = logging.getLogger('cbb.cert-check')
-    logger.setLevel('INFO')
+    level = logging.getLevelName('INFO')
+    logger.setLevel(level)
 
     def __init__(self, url):
         # see also https://stackoverflow.com/a/7691293
         self.url = url
-        print(requests.get(self.url))
-
         self.domain = self._get_domain()
-        self.content = self._get_content()
+        self.content = None
 
     def check(self):
         '''Main public check function.'''
+        self.logger.info('Testing connection ...')
+        if not self._test_connection():
+            return False
+
+        self.content = self._get_content()
         self.logger.info('Initialising TLS/SSL certificate check ...')
         if not self.domain:
-            return
+            return False
+
+        subj_name = self._verify_subject()
+        if not subj_name:
+            san_list = self._get_extensions()
+            for name in san_list:
+                if self.domain in name:
+                    subj_name = True
 
         return all((
             self._verify_version(),
             self._verify_date(),
-            self._verify_subject(),
+            subj_name,
 #            self._verify_issuer(),
-            self._get_extensions(),
         ))
 
     def _get_domain(self):
@@ -58,6 +70,19 @@ class CertChecker:
         print(domain)
         return domain
 
+    def _test_connection(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        try:
+            s.connect((self.domain, 443))
+            return True
+        except:
+            print('Cannot connect to port 443.')
+            self.logger.info('Cannot connect to port 443.')
+            return False
+
+        s.close()
+
     def _get_content(self):
         '''Decrypts certificate and returns `Certificate` object.
         See https://cryptography.io/en/latest/x509/reference/#x-509-certificate-object
@@ -67,12 +92,11 @@ class CertChecker:
         print('Getting certificate ...')
         # https://stackoverflow.com/a/16899645
         cert = ssl.get_server_certificate((self.domain, 443))
-        print('cert done')
         bycert = cert.encode('utf-8')
-        print('bcert done')
         content = x509.load_pem_x509_certificate(bycert, default_backend())
         self.logger.info(content)
-        print('Done')
+        print('OK')
+        self.logger.info('OK')
         return content
 
     def _verify_version(self):
@@ -82,7 +106,7 @@ class CertChecker:
         try:
             version = self.content.version
             self.logger.info('Version: {0}'.format(version))
-            print('Done')
+            print('OK')
             return True
         except x509.InvalidVersion as ex:
             self.logger.warning('Invalid certificate version: {0}'.format(ex))
@@ -100,7 +124,7 @@ class CertChecker:
         # check the cert
         if not_before <= datetime.datetime.utcnow() < not_after:
             self.logger.info('Dates - OK')
-            print('Done')
+            print('OK')
             return True
 
         self.logger.warning('Certificate has expired: %s - %s'
@@ -121,22 +145,24 @@ class CertChecker:
             # check if domain name coinsides with the Common Name
             if key == 'commonName' and self.domain.lower() in val.lower():
                 self.logger.info(self.domain, val)
-                print('Done')
+                print('OK')
                 return True
 
-        return
+        print('No CN field in the certificate.')
+        self.logger.info('No CN field in the certificate.')
+        return False
 
     # TODO
     def _get_extensions(self):
         '''Gets data from extensions.'''
+        print('Verifying certificate extensions ...')
         self.logger.info('Verifying certificate extensions ...')
         san = self.content.extensions.get_extension_for_class(
             x509.SubjectAlternativeName)
-        self.logger.info(
-            'SAN: {0}'.format(san.value.get_values_for_type(x509.DNSName)))
-        print(
-            'SAN: {0}'.format(san.value.get_values_for_type(x509.DNSName)))
-        return True
+        san_list = san.value.get_values_for_type(x509.DNSName)
+        self.logger.info('SAN: {0}'.format(san_list))
+        print('SAN: {0}'.format(san_list))
+        return san_list
 
     # TODO
     def _verify_issuer(self):
@@ -146,3 +172,26 @@ class CertChecker:
             key = attribute.oid._name
             val = attribute.value
             self.logger.info('{0}: {1}'.format(key, val))
+
+    def _scan_ports(self):
+        ''' Scans ports of the server.'''
+        print('Scanning ports ...')
+        self.logger.info('Scanning ports ...')
+        nm = nmap.PortScanner()
+        nm.scan(self.domain)
+        print(nm)
+        hosts = nm.all_hosts()
+        print(hosts)
+        ip = hosts[0]
+        print(ip)
+        try:
+            serv_state = nm[ip].tcp(443)['state']
+            if serv_state == 'up':
+                print('Server is UP.')
+                return True
+            else:
+                print('Server is not accessible.')
+                return False
+        except KeyError:
+            print('Not an HTTPS domain.')
+            return False
--- validation_api/cert_check/test.py
+++ validation_api/cert_check/test.py
@@ -1,16 +1,26 @@
+import nmap
 import socket
 import ssl
 
 
 domain = 'www.112misdaad.nl'
+#domain = '165.231.102.206'
+#domain = 'www.adidaisclimacool.nl'
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-context.verify_mode = ssl.CERT_REQUIRED
-context.check_hostname = True
-context.load_default_certs()
-print(context)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print(s)
-ssl_sock = context.wrap_socket(s, server_hostname=domain)
-print(ssl_sock.connect((domain, 443)))
+nm = nmap.PortScanner()
+nm.scan(domain)
+#print(nm.scaninfo())
+print(nm.all_hosts())
+print(nm['165.231.102.206'].hostnames())
+print(nm['165.231.102.206'].state())
+print(nm['165.231.102.206'].tcp(80))
 
+#context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+#context.verify_mode = ssl.CERT_REQUIRED
+#context.check_hostname = True
+#context.load_default_certs()
+#print(context)
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#print(s)
+#ssl_sock = context.wrap_socket(s, server_hostname=domain)
+#print(ssl_sock.connect((domain, 443)))
